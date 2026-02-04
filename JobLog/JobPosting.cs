2ee31207c95e34c84d4deca8b21c105d49794229
manveti <manveti@users.noreply.github.com>
2026-02-03 20:44:20 -0800

    Initial project commit with core data structures

---
+++ JobLog/JobPosting.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JobLog {
+    [Serializable]
+    public class JobEvent {
+        public DateTime date;
+        public string description;
+
+        public JobEvent(DateTime date, string description) {
+            this.date = date;
+            this.description = description;
+        }
+    }
+
+    [Serializable]
+    public class JobPosting {
+        public string employer;
+        public string recruiter;
+        public string title;
+        public string salary;
+        public string description;
+        public string notes;
+        public List<string> urls;
+        public DateTime? posted_date;
+        public DateTime saved_date;
+        public DateTime? applied_date;
+        public List<JobEvent> timeline;
+        public DateTime? closed_date;
+        public string closed_reason;
+
+        public DateTime opened_date { get => this.posted_date ?? this.saved_date; }
+        public bool applied { get => this.applied_date.HasValue; }
+        public bool closed { get => this.closed_date.HasValue; }
+        public bool pending { get => !this.applied && !this.closed; }
+        public bool in_flight { get => this.applied && !this.closed; }
+
+        public JobPosting() {
+            this.urls = new List<string>();
+            this.saved_date = DateTime.Today;
+            this.timeline = new List<JobEvent>();
+        }
+
+        public JobPosting(
+            string employer,
+            string recruiter,
+            string title,
+            string salary,
+            string description,
+            string notes,
+            IEnumerable<string> urls = null,
+            DateTime? posted_date = null,
+            DateTime? saved_date = null
+        ) {
+            this.employer = employer;
+            this.recruiter = recruiter;
+            this.title = title;
+            this.salary = salary;
+            this.description = description;
+            this.notes = notes;
+            if (urls == null) {
+                this.urls = new List<string>();
+            }
+            else {
+                this.urls = new List<string>(urls);
+            }
+            this.posted_date = posted_date;
+            this.saved_date = saved_date ?? DateTime.Today;
+            this.timeline = new List<JobEvent>();
+        }
+
+        public void addUrl(string url) {
+            this.urls.Add(url);
+        }
+
+        public void updateUrl(int idx, string url) {
+            this.urls[idx] = url;
+        }
+
+        public void removeUrl(int idx) {
+            this.urls.RemoveAt(idx);
+        }
+
+        public void addEvent(string description, DateTime? date = null) {
+            this.addEvent(new JobEvent(date ?? DateTime.Today, description));
+        }
+
+        public void addEvent(JobEvent evt) {
+            int idx = this.timeline.FindIndex((e) => e.date > evt.date);
+            if (idx < 0) {
+                this.timeline.Add(evt);
+            }
+            else {
+                this.timeline.Insert(idx, evt);
+            }
+        }
+
+        public void updateEvent(int idx, string description, DateTime? date = null) {
+            JobEvent evt = this.timeline[idx];
+            if (description != null) {
+                evt.description = description;
+            }
+            if ((date.HasValue) && (date.Value != evt.date)) {
+                evt.date = date.Value;
+                // remove and re-add so that we maintain the right order after date change
+                this.timeline.RemoveAt(idx);
+                this.addEvent(evt);
+            }
+        }
+
+        public void removeEvent(int idx) {
+            this.timeline.RemoveAt(idx);
+        }
+
+        public void close(string reason, DateTime? date = null) {
+            this.closed_date = date ?? DateTime.Today;
+            this.closed_reason = reason;
+        }
+    }
+}
---
+++ JobLog/JobTracker.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Xml;
+
+namespace JobLog {
+    [Serializable]
+    public class CompanyInfo {
+        public string name;
+        public string description;
+
+        public CompanyInfo(string name, string description) {
+            this.name = name;
+            this.description = description;
+        }
+    }
+
+    class JobTracker {
+        public string data_dir;
+        protected string jobs_dir;
+        protected string blacklist_path;
+        public Dictionary<Guid, JobPosting> jobs;
+        public List<CompanyInfo> blacklist;
+
+        public JobTracker(string data_dir) {
+            this.data_dir = data_dir;
+            this.jobs_dir = Path.Join(data_dir, "jobs");
+            this.blacklist_path = Path.Join(data_dir, "blacklist.xml");
+            this.loadJobs();
+            this.loadBlacklist();
+        }
+
+        public void loadJobs() {
+            this.jobs = new Dictionary<Guid, JobPosting>();
+            if (!Directory.Exists(this.jobs_dir)) {
+                // nothing to load
+                return;
+            }
+            foreach (string fn in Directory.EnumerateFiles(this.jobs_dir, "*.job")) {
+                Guid guid;
+                try {
+                    guid = Guid.Parse(Path.GetFileNameWithoutExtension(fn));
+                }
+                catch (FormatException) {
+                    // this isn't a job file; skip it
+                    continue;
+                }
+                DataContractSerializer serializer = new DataContractSerializer(typeof(JobPosting));
+                using (FileStream f = new FileStream(fn, FileMode.OpenOrCreate)) {
+                    XmlDictionaryReader xmlReader = XmlDictionaryReader.CreateTextReader(f, new XmlDictionaryReaderQuotas());
+                    this.jobs[guid] = (JobPosting)(serializer.ReadObject(xmlReader, true));
+                }
+            }
+        }
+
+        void loadBlacklist() {
+            if (!File.Exists(this.blacklist_path)) {
+                // nothing to load
+                this.blacklist = new List<CompanyInfo>();
+                return;
+            }
+            DataContractSerializer serializer = new DataContractSerializer(typeof(List<CompanyInfo>));
+            using (FileStream f = new FileStream(this.blacklist_path, FileMode.OpenOrCreate)) {
+                XmlDictionaryReader xmlReader = XmlDictionaryReader.CreateTextReader(f, new XmlDictionaryReaderQuotas());
+                this.blacklist = (List<CompanyInfo>)(serializer.ReadObject(xmlReader, true));
+            }
+        }
+
+        void saveJob(Guid guid) {
+            if (!this.jobs.ContainsKey(guid)) {
+                return;
+            }
+            Directory.CreateDirectory(this.jobs_dir);
+            string path = Path.Join(this.jobs_dir, guid.ToString() + ".job");
+            using (FileStream f = new FileStream(path, FileMode.Create)) {
+                DataContractSerializer serializer = new DataContractSerializer(typeof(JobPosting));
+                serializer.WriteObject(f, this.jobs[guid]);
+            }
+        }
+
+        void saveBlacklist() {
+            Directory.CreateDirectory(this.data_dir);
+            using (FileStream f = new FileStream(this.blacklist_path, FileMode.Create)) {
+                DataContractSerializer serializer = new DataContractSerializer(typeof(List<CompanyInfo>));
+                serializer.WriteObject(f, this.blacklist);
+            }
+        }
+    }
+}
